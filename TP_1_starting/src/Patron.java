f754ea2c89beb52176a88663953094a1e2aae0a2
zsbenq <shuangcool314@gmail.com>
2017-02-16 16:22:07 -0600

    Update Patron.java

--- TP_1_starting/src/Patron.java
+++ TP_1_starting/src/Patron.java
@@ -4,7 +4,7 @@ public class Patron
 {
 	private String name;
 	private String patronID;
-	private ArrayList<Copy> copiesOut; //change1
+	private ArrayList<Copy> copiesOut; //change2
 
 	public Patron(String n, String id)
 	{
